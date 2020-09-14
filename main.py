1c9867a1ecf30fb500d8cede370fa110b4d1e5d1
Joe Tseng <jtsengorg@gmail.com>
2020-09-14 00:10:19 -0400

    [WIP] Added template for index, includes user login and url forms.

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def read_root():
-    results = {"Hello": "World"}
-    return results
+    # results = {"Hello": "World"}
+    # return results
+    return templates.TemplateResponse("index.html", {"request": {}})
+
+@app.get("/bec")
+def read_bec():
+    return "I ❤️ Bec"
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
